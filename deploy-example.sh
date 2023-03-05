b678fcfcf95ef2c56dd89ef0503b34aba7f38822
harold.caicedo <harold.caicedo>
2023-03-05 13:56:55 +0100

    feat: (deploy) adds scripts to create and deploy the kubernetes manifests in the local kubernetes cluster with kind

---
+++ deploy-example.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -o errexit
+
+# create a new cluster with local repository
+./cluster-test.sh
+
+# build the docker image
+docker build -t note-sample-app:1
+
+# push the docker image
+docker push note-sample-app:1
+
+# apply manifest inside the cluster
+kubectl apply -f deployment.yaml
+
+# get all objects inside the cluster in the target namespace
+kubectl get all
+
+
